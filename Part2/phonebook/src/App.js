15712d0c7f37c167600f87cb6dd9659d5feac05d
Akash Mehta <mehtaakash966@gmail.com>
2020-07-16 12:49:35 +0100

    completed phonebook step 6 (2.11)

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 
 const App = () => {
@@ -9,6 +10,16 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [filtered, setFiltered] = useState('')
+
+
+useEffect(()=>{
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response =>{
+      setPersons(response.data)
+    })
+}, [])
+
 
   const addPerson = (event) => {
     event.preventDefault();
