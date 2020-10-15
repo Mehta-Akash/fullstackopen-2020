05864bb61546c978330f7714138103de79005ea3
Akash Mehta <mehtaakash966@gmail.com>
2020-10-16 00:23:00 +0100

    Blog step 2 complete

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,47 +1,41 @@
-
+const config = require('./utils/config');
 const express = require('express');
 
 const app = express();
 const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
+// const morgan = require('morgan');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB', error.message);
+  });
 
 app.use(cors());
-// app.use(express.static('build'));
+app.use(express.static('build'));
 app.use(express.json());
-app.use(
-  morgan(':method :url :status :res[content-length] :response-time ms :data'),
-);
+app.use(middleware.requestLogger);
 
-morgan.token('data', (req) => JSON.stringify(req.body));
+// app.use(
+//   morgan(':method :url :status :res[content-length] :response-time ms :data')
+// );
+// morgan.token('data', (req) => JSON.stringify(req.body));
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+app.use('/api/blogs', blogsRouter);
 
-const Blog = mongoose.model('Blog', blogSchema);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const { PORT } = process.env;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,47 +1,7 @@
+const app = require('./app');
 const config = require('./utils/config');
-
-const express = require('express');
-
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-
-app.use(cors());
-// app.use(express.static('build'));
-app.use(express.json());
-app.use(
-  morgan(':method :url :status :res[content-length] :response-time ms :data')
-);
-
-morgan.token('data', (req) => JSON.stringify(req.body));
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+const logger = require('./utils/logger');
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
