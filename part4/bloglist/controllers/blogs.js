e245b79af76771a85c7fe003ca64ba6b552969e1
Akash Mehta <mehtaakash966@gmail.com>
2020-10-24 11:44:53 +0100

    exercise 4.14 complete

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -13,10 +13,26 @@ blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body);
   const savedBlog = await blog.save();
   response.json(savedBlog.toJSON());
+});
 
-  // blog.save().then((result) => {
-  //   response.status(201).json(result);
-  // });
+blogsRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const { body } = request;
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  const blogUpdate = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(blogUpdate);
 });
 
 module.exports = blogsRouter;
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -13,64 +13,82 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test('blogs returned as json and returns correct amount', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+describe('when there are initially some notes', () => {
+  test('blogs returned as json and returns correct amount', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
 
-  const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(helper.initialBlog.length);
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(helper.initialBlog.length);
+  });
+
+  test('id is the unique identifier', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body[0].id).toBeDefined();
+  });
 });
 
-test('id is the unique identifier', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body[0].id).toBeDefined();
+describe('addition of a new note', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Mindscape',
+      author: 'Sean Carroll',
+      url: 'https://www.preposterousuniverse.com/podcast/',
+      likes: 7,
+    };
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    const title = blogsAtEnd.map((r) => r.title);
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlog.length + 1);
+    expect(title).toContain('Mindscape');
+  });
+
+  test('if likes property is empty, defaults to 0', async () => {
+    const newBlog = {
+      title: 'Mindscape',
+      author: 'Sean Carroll',
+      url: 'https://www.preposterousuniverse.com/podcast/',
+    };
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const response = await api.get('/api/blogs');
+    expect(response.body[helper.initialBlog.length].likes).toBe(0);
+  });
+
+  test('if title and url missing in post request, returns 400', async () => {
+    const newBlog = {
+      author: 'Sean Carroll',
+      likes: 7,
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(400);
+  });
 });
 
-test.only('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Mindscape',
-    author: 'Sean Carroll',
-    url: 'https://www.preposterousuniverse.com/podcast/',
-    likes: 7,
-  };
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+describe('deletion of a blog', () => {
+  test.only('succeeds with status code 204 if id is valid', async () => {
+    const blogsInDb = await helper.blogsInDb();
+    const blogToDelete = blogsInDb[0];
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  const title = blogsAtEnd.map((r) => r.title);
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd.length).toBe(helper.initialBlog.length - 1);
 
-  expect(blogsAtEnd).toHaveLength(helper.initialBlog.length + 1);
-  expect(title).toContain('Mindscape');
-});
-
-test('if likes property is empty, defaults to 0', async () => {
-  const newBlog = {
-    title: 'Mindscape',
-    author: 'Sean Carroll',
-    url: 'https://www.preposterousuniverse.com/podcast/',
-  };
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-
-  const response = await api.get('/api/blogs');
-  expect(response.body[helper.initialBlog.length].likes).toBe(0);
-});
-
-test('if title and url missing in post request, returns 400', async () => {
-  const newBlog = {
-    author: 'Sean Carroll',
-    likes: 7,
-  };
-
-  await api.post('/api/blogs').send(newBlog).expect(400);
+    const title = blogsAtEnd.map((t) => t.title);
+    expect(title).not.toContain(blogToDelete.title);
+  });
 });
 
 afterAll(() => {
