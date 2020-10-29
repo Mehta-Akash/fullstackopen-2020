c831819b9769132540628da5ecf4305305ca74a5
Akash Mehta <mehtaakash966@gmail.com>
2020-10-29 21:22:33 +0000

    exercise 4.6 complete

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+let _ = require('lodash');
+
 const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => {
@@ -13,14 +15,24 @@ const favouriteBlog = (blogs) => {
     newObj.likes = blog.likes;
     return newObj;
   });
-  const mostLiked = reformatedBlogs.reduce(
-    (most, current) => (current.likes > most.likes ? current : most),
+  const mostLiked = reformatedBlogs.reduce((most, current) =>
+    current.likes > most.likes ? current : most
   );
   return mostLiked;
+};
+
+const mostBlogs = (blogs) => {
+  const authors = blogs.map((blog) => blog.author);
+  const result = _.chain(authors).countBy().toPairs().maxBy(_.last).value();
+  return {
+    author: result[0],
+    blogs: result[1],
+  };
 };
 
 module.exports = {
   dummy,
   totalLikes,
   favouriteBlog,
+  mostBlogs,
 };
