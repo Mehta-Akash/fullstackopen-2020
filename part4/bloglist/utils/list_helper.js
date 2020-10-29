8050a580d6fa9b6d0fcf16548dd043ff8ca7fc04
Akash Mehta <mehtaakash966@gmail.com>
2020-10-29 22:19:31 +0000

    exercise 4.7 complete

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -30,9 +30,27 @@ const mostBlogs = (blogs) => {
   };
 };
 
+const mostLikes = (blogs) => {
+  const authors = blogs.map((blog) => blog.author);
+  const uniqeAuthors = _.uniq(authors);
+  const grouped = _.groupBy(blogs, 'author');
+
+  const totalOfLikesArr = uniqeAuthors.map((x) => {
+    const b = grouped[x].reduce((sum, value) => sum + value.likes, 0);
+    return b;
+  });
+
+  const maxIndex = totalOfLikesArr.indexOf(Math.max(...totalOfLikesArr));
+  return {
+    author: uniqeAuthors[maxIndex],
+    likes: totalOfLikesArr[maxIndex],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favouriteBlog,
   mostBlogs,
+  mostLikes,
 };
